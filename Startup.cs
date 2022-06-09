b765d2a2de7d49a205a4ce6ce6ce5bcfd31f6874
matkhau146 <69472182+matkhau146@users.noreply.github.com>
2022-06-09 13:38:25 +0700

    cham diem

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NguyenVietHoang_BigSchool.Startup))]
+namespace NguyenVietHoang_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
